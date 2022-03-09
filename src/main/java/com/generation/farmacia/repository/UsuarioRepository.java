8baf708a980e1f3b888d37b4df35f1ecaa43dca7
Matheus Oliveira <97600307+MatheusOliveira-1@users.noreply.github.com>
2022-03-08 22:33:43 -0300

    finalizada a camada Security

--- src/main/java/com/generation/farmacia/repository/UsuarioRepository.java
+++ src/main/java/com/generation/farmacia/repository/UsuarioRepository.java
@@ -1,5 +1,15 @@
 package com.generation.farmacia.repository;
 
-public interface UsuarioRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.farmacia.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository <Usuario, Long>{
+	
+	public Optional<Usuario> findByEmail (String email);
 
 }
